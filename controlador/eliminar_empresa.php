244635c9b7ed59a1c24f01d401c626f9fc01c7b9
Jorge Manrique <58120477+yorchmx@users.noreply.github.com>
2022-08-13 17:01:18 -0500

    correción en eliminar

--- controlador/eliminar_empresa.php
+++ controlador/eliminar_empresa.php
@@ -4,7 +4,7 @@
         $id=$_GET["id"];
         $sql=$conexion->query(" delete from empresas where id=$id ");
         if ($sql==1) {
-            echo '<div>Empresa elininada correctamente</div>';
+            echo '<div>Empresa eliminada correctamente</div>';
         } else {
             echo '<div>Error al eliminar</div>';
         }
